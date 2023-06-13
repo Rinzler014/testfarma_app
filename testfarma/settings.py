c8db76811cae8eef4ad34557b6d9e8d907cd61c3
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2023-06-13 16:42:27 -0600

    Fix3

--- testfarma/settings.py
+++ testfarma/settings.py
@@ -61,6 +61,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
